f712db8f5c7492f40a5b03b70a95bca36f415087
Sawex <danil27zp@gmail.com>
2019-09-06 01:36:18 +0300

    changes mobile menu and added hovers for all the necessary elements

--- scripts/common.js
+++ scripts/common.js
@@ -1,7 +1,9 @@
 const hambButton = document.querySelector('.hamburger');
 const mobileMenu = document.querySelector('.header__mobile-menu');
+const mobileMenuWrapper = document.querySelector('.mobile-menu-wrapper');
 
 hambButton.addEventListener('click', () => {
   hambButton.classList.toggle('is-active');
   mobileMenu.classList.toggle('header__mobile-menu--visible');
+  mobileMenuWrapper.classList.toggle('mobile-menu-wrapper--pos');
 });
